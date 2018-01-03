b10cb683de4ff734eafeee98f4f5f5c471f69646
Eshan Das <eshandasnit@gmail.com>
2018-01-03 15:57:36 +0530

    Migration fixes

--- django_project/kv_settings/models.py
+++ django_project/kv_settings/models.py
@@ -2,18 +2,6 @@ from __future__ import unicode_literals
 
 from django.db import models
 
-import json
-
-
-class KeyValueSettingManager(models.Manager):
-    def get_value(self, key):
-        setting = super(KeyValueSettingManager, self).get_queryset().get(key=key)
-        return setting.value
-
-    def get_dict_value(self, key):
-        setting = super(KeyValueSettingManager, self).get_queryset().get(key=key)
-        return json.loads(setting.value)
-
 
 class KeyValueSetting(models.Model):
     key = models.CharField(
@@ -26,8 +14,6 @@ class KeyValueSetting(models.Model):
         auto_now_add=True)
     updated_on = models.DateTimeField(
         auto_now=True)
-
-    objects = KeyValueSettingManager()
 
     def __str__(self):
         return self.key
